202d388da96b07da605e87641e937385aca72112
Jefferson Chaves Gomes <jefferson.chaves@gmail.com>
2016-02-22 23:08:00 -0300

    Adding a TEMP fake code to provide support to MAC OSs

--- src/main/java/br/unb/cic/bionimbus/plugin/linux/LinuxGetInfo.java
+++ src/main/java/br/unb/cic/bionimbus/plugin/linux/LinuxGetInfo.java
@@ -1,25 +1,23 @@
 /*
-    BioNimbuZ is a federated cloud platform.
-    Copyright (C) 2012-2015 Laboratory of Bioinformatics and Data (LaBiD), 
-    Department of Computer Science, University of Brasilia, Brazil
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ * BioNimbuZ is a federated cloud platform.
+ * Copyright (C) 2012-2015 Laboratory of Bioinformatics and Data (LaBiD),
+ * Department of Computer Science, University of Brasilia, Brazil
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package br.unb.cic.bionimbus.plugin.linux;
 
-import br.unb.cic.bionimbus.plugin.PluginInfo;
-import br.unb.cic.bionimbus.plugin.PluginService;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
@@ -30,48 +28,44 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import org.apache.commons.lang3.SystemUtils;
 import org.codehaus.jackson.map.ObjectMapper;
+
+import br.unb.cic.bionimbus.plugin.PluginInfo;
+import br.unb.cic.bionimbus.plugin.PluginService;
 
 public class LinuxGetInfo implements Callable<PluginInfo> {
 
     private static final String SERVICE_DIR = "services";
-
     public static final String PATH = "files";
-
     public static final String CORES = "dstat -cf";
-
     public static final String CPUMHz = "grep -m 1 MHz /proc/cpuinfo";
-    
     public static final String CPUGHz = "grep -m 1 GHz /proc/cpuinfo";
-
     public static final String MemTotal = "grep -m 1 MemTotal /proc/meminfo";
-
     public static final String MemFree = "grep -m 1 MemFree /proc/meminfo";
-
     private final PluginInfo pluginInfo = new PluginInfo();
 
     public LinuxGetInfo() {
     }
-
     @Override
     public PluginInfo call() {
-        
         try {
             getCpuInfo();
             getMemoryInfo();
             getDiskInfo();
             getServices();
             getUptime();
-        }catch(Exception ex){
+        } catch (Exception ex) {
             System.out.print(ex);
         }
         return pluginInfo;
     }
-
-    
     /**
-     * Obtem as informações do processador do recurso e realiza o setter dessa informações na classe pluginInfo.
-     * Número de cores, frequencia do processador(GHz) e quantidade de cores ocupados.
+     * Obtem as informações do processador do recurso e realiza o setter dessa
+     * informações na classe pluginInfo.
+     * Número de cores, frequencia do processador(GHz) e quantidade de cores
+     * ocupados.
      */
     private void getCpuInfo() {
         int nCpus = Runtime.getRuntime().availableProcessors();
@@ -80,78 +74,102 @@ public class LinuxGetInfo implements Callable<PluginInfo> {
         pluginInfo.setNumNodes(1);
         pluginInfo.setNumOccupied(getCoresOccupied(nCpus));
         String cpuInfo = execCommand(CPUMHz);
+        if (!SystemUtils.IS_OS_LINUX) {
+            // TODO: TEMP CODE - begin
+            // Adding fake code to provide in the futere a support to others OSs
+            cpuInfo = ":2400";
+            // TODO: TEMP CODE - end
+        }
         pluginInfo.setCurrentFrequencyCore((new Double(cpuInfo.substring(cpuInfo.indexOf(":") + 1, cpuInfo.length()).trim())) / 100000);
         cpuInfo = execCommand(CPUGHz);
-        Double freq = new Double(cpuInfo.substring(cpuInfo.indexOf("@") + 1, cpuInfo.length()-3).trim())*1000000000;
+        if (!SystemUtils.IS_OS_LINUX) {
+            // TODO: TEMP CODE - begin
+            // Adding fake code to provide in the futere a support to others OSs
+            cpuInfo = "@2.4mac";
+            // TODO: TEMP CODE - end
+        }
+        Double freq = new Double(cpuInfo.substring(cpuInfo.indexOf("@") + 1, cpuInfo.length() - 3).trim()) * 1000000000;
         pluginInfo.setFactoryFrequencyCore(freq);
     }
-
     /**
-     * Retorna o número de cores ocupados no recuso caso seu processamento estaja acima de 70 porcento.
+     * Retorna o número de cores ocupados no recuso caso seu processamento
+     * estaja acima de 70 porcento.
+     * 
      * @return número de cores ocupados
      */
-    private int getCoresOccupied(int numCpu){
-        int nCpuOccupied=0;
+    private int getCoresOccupied(int numCpu) {
+        if (!SystemUtils.IS_OS_LINUX) {
+            // TODO: TEMP CODE - begin
+            // Adding fake code to provide in the futere a support to others OSs
+            return 1;
+            // TODO: TEMP CODE - end
+        }
+        int nCpuOccupied = 0;
         try {
-            InputStreamReader read ;
-            BufferedReader buffer ;
-            List<Integer> linesCPU= new ArrayList<Integer>(numCpu);
-            String[] columns,lines;
+            InputStreamReader read;
+            BufferedReader buffer;
+            List<Integer> linesCPU = new ArrayList<Integer>(numCpu);
+            String[] columns, lines;
             String line;
-            int count=0,i=0;
-
+            int count = 0, i = 0;
             Process p = Runtime.getRuntime().exec("dstat -cf");
             read = new InputStreamReader(p.getInputStream());
             buffer = new BufferedReader(read);
-            
-            // magic number                                 numCpu+(4)
-            while((line = buffer.readLine())!=null && count<(numCpu+4)){
-                if(count>=3){
+            // magic number numCpu+(4)
+            while ((line = buffer.readLine()) != null && count < (numCpu + 4)) {
+                if (count >= 3) {
                     columns = line.trim().split(":");
-                    for(int j=0; j<numCpu;j++){
+                    for (int j = 0; j < numCpu; j++) {
                         lines = columns[j].trim().split(" ");
-                        if(i!=0){
-                            linesCPU.set(j,new Integer(lines[0])+linesCPU.get(j));
-                        }else{
-                            linesCPU.add(j,new Integer(lines[0]));
-
+                        if (i != 0) {
+                            linesCPU.set(j, new Integer(lines[0]) + linesCPU.get(j));
+                        } else {
+                            linesCPU.add(j, new Integer(lines[0]));
                         }
-
                     }
-                i++;
+                    i++;
                 }
                 count++;
             }
-            //finaliza a execução
+            // finaliza a execução
             p.destroy();
-
-            for(i=0; i<numCpu;i++){
-                if((linesCPU.get(i)/3)>80){
+            for (i = 0; i < numCpu; i++) {
+                if ((linesCPU.get(i) / 3) > 80) {
                     nCpuOccupied++;
                 }
             }
-
         } catch (IOException ex) {
             Logger.getLogger(LinuxGetInfo.class.getName()).log(Level.SEVERE, null, ex);
         }
-
-
         return nCpuOccupied;
     }
-    
     /**
-     * Obtem as informações da memória RAM do recurso e realiza o setter dessa informações na classe pluginInfo.
-     * Memória total e memória livre em GigaBytes(Retorno da eexcução em KB / 1024²).
+     * Obtem as informações da memória RAM do recurso e realiza o setter dessa
+     * informações na classe pluginInfo.
+     * Memória total e memória livre em GigaBytes(Retorno da eexcução em KB /
+     * 1024²).
      */
     private void getMemoryInfo() {
         String mem = execCommand(MemTotal);
+        // TODO: TEMP CODE - begin
+        // Adding fake code to provide in the futere a support to others OSs
+        if (!SystemUtils.IS_OS_LINUX) {
+            mem = ":" + 4 * 1048576;
+        }
+        // TODO: TEMP CODE - end
         pluginInfo.setMemoryTotal((new Double(mem.substring(mem.indexOf(":") + 1, mem.length() - 2).trim()) / 1048576));
         mem = execCommand(MemFree);
+        // TODO: TEMP CODE - begin
+        // Adding fake code to provide in the futere a support to others OSs
+        if (!SystemUtils.IS_OS_LINUX) {
+            mem = ":" + 2 * 1048576;
+        }
+        // TODO: TEMP CODE - end
         pluginInfo.setMemoryFree((new Double(mem.substring(mem.indexOf(":") + 1, mem.length() - 2).trim()) / 1048576));
     }
-
     /**
-     * Obtem as informações do espaço e, disco do recurso e realiza o setter dessa informações na classe pluginInfo.
+     * Obtem as informações do espaço e, disco do recurso e realiza o setter
+     * dessa informações na classe pluginInfo.
      * Espaço total e espaço livre.
      */
     private void getDiskInfo() {
@@ -164,11 +182,9 @@ public class LinuxGetInfo implements Callable<PluginInfo> {
             }
         }
     }
-
     private void getServices() throws Exception {
         final List<PluginService> list = new CopyOnWriteArrayList<PluginService>();
         File dir = new File(SERVICE_DIR);
-
         if (dir.isDirectory()) {
             for (File file : dir.listFiles()) {
                 if (file.isFile() && file.canRead() && file.getName().endsWith(".json")) {
@@ -178,35 +194,28 @@ public class LinuxGetInfo implements Callable<PluginInfo> {
                 }
             }
         }
-
         pluginInfo.setServices(list);
     }
-    
-      private void getUptime(){
+    private void getUptime() {
         pluginInfo.setUptime(System.currentTimeMillis());
     }
-
-
     /**
      * Retorna os valores da execução do comando informado pelo parâmetro
      *
-     * @param Command comando a ser executado
+     * @param Command
+     *            comando a ser executado
      * @return string resultado da execução
      */
     private String execCommand(String Command) {
         String line = null;
         InputStreamReader read;
         try {
-
             read = new InputStreamReader(Runtime.getRuntime().exec(Command).getInputStream());
             BufferedReader buffer = new BufferedReader(read);
-
             line = buffer.readLine();
-
         } catch (IOException ex) {
             Logger.getLogger(LinuxGetInfo.class.getName()).log(Level.SEVERE, null, ex);
         }
-
         return line;
     }
 }
